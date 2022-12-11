0905f0672bb06c338266231768f1dabd07c2089b
Adenote <Agbajeanofiazeez@gmail.com>
2022-12-11 13:57:58 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,14 +5,14 @@
  *
  *   *
  *
- *    * Return: Always 0 (Success)
+ *    * Return: Always 0
  *
  *     */
 
 int main(void)
 
 {
-	puts("\"rogramming is like building a multilingual puzzle");
+	puts("\"programming is like building a multilingual puzzle");
 
 	    return (0);
 
