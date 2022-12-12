32538002b8326c9ccd1b68426144e0564097950b
Adenote <Agbajeanofiazeez@gmail.com>
2022-12-12 07:22:02 -0800

    101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,10 @@
 
 /**
  * main -Entry
- * Return: Always 0
+ * Return: 1
  */
 int main(void)
 {
-	write(1, "and the piece of art is useful\"-Dora Korper, 2015-10-19\n", 59);
-	return (0);
+	write(1, "and the piece of art is useful\"- Dora Korper, 2015-10-19\n", 59);
+	return (1);
 }
