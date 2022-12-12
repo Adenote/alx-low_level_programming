e62c48473a35d14a753b4a354a3edfe06e1a3275
Adenote <Agbajeanofiazeez@gmail.com>
2022-12-12 07:18:14 -0800

    101-quote.

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main -Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	write(1, "and the piece of art is useful\"-Dora Korper, 2015-10-19\n", 59);
+	return (0);
+}
