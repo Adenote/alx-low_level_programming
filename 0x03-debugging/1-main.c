7850eae7f87b4da8abc484a5c0c4e994185875f4
Adenote <Agbajeanofiazeez@gmail.com>
2022-12-15 18:22:08 -0800

    main 1 and 2

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - causing the output to go into an infinite loop.
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("infinte loop incoming:\n");
+	i = 0;
+
+	/**
+	 *while (i < 10)
+	 *{
+	 *putchar
+	 *}
+	 */
+	printf("infinite loop avoided! \\ 0/in");
+	return (0);
+}
+
+
+
