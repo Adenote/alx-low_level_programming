d0d163422a799e4388a251cf6fe7e7b05e82b548
Adenote <Agbajeanofiazeez@gmail.com>
2022-12-15 19:11:03 -0800

    largest number

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -11,6 +11,7 @@ int main(void)
 
 	i = 0;
 	positive_negative(i);
+
 	return (0);
 }
 
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,9 +4,9 @@
 #include <stdio.h>
 
 void posittive_or_negative(int i);
-int laegest_number(int a. int b. int c);
-void print_remaining_days(int month. int day. int year);
-int convert_day(int month. int day);
+int laegest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
 void print_time_table(int n);
 
 #endif
