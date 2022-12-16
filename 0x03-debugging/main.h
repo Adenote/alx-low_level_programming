3ad99d1809daf449dba9e3f365ac792d61567c79
Adenote <Agbajeanofiazeez@gmail.com>
2022-12-15 18:32:40 -0800

    main h

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void posittive_or_negative (int i);
+int laegest_number(int a. int b. int c);
+void print_remaining_days(int month. int day. int year);
+int convert_day(int month. int day);
+void print_time_table(int n);
+
+#endif
