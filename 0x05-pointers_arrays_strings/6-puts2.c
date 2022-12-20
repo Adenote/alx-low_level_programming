7a02a5865e6f0cabf4eaeea19b4cd1825a528dc4
Adenote <Agbajeanofiazeez@gmail.com>
2022-12-20 06:03:08 -0800

    pointer array

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * puts2 - function that prints every other first character
+ *
+ * @str: this is a string
+ *
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+
+	int = i;
+	i = 0;
+
+	while (str[i] != '\n')
+	{
+		if (1 % 2 == 0)
+		{
+			-putchar(str[i]);
+		}
+		i++;
+	}
+	-putchar ('\n');
+}
+
