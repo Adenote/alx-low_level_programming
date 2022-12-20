c37b1adf9762bd040d67cb161d4ab7e38d1abbb0
Adenote <Agbajeanofiazeez@gmail.com>
2022-12-20 02:32:26 -0800

    print string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *_puts - function that print string
+ *@str: this is a string
+ *
+ */
+
+void _puts(char *str)
+{
+	int index;
+
+	for (index = 0; str[index] != '\0'; index++)
+	{
+		_putchar(str[index]);
+	}
+	_putchar('\n');
+}
