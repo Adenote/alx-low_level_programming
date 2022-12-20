ab3c922abdfd851466b6fe193e9273c08f8516cd
Adenote <Agbajeanofiazeez@gmail.com>
2022-12-20 00:25:52 -0800

    copy string

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,15 +1,16 @@
 #include "main.h"
 /**
  * _strcpy - function that copies the string pointed
- * @desr: this is a charater
+ * @dest: this is a charater
  * @src: this is a character
  *
  * Return: this return copy
  */
 
-char _strcpy(char *dest, char *src)
+char strcpy(char *dest, char *src)
 {
 	char *start = dest;
+
 	while (*src != '\0')
 	{
 		*dest = *src;
