e42e85fe7b614869a2e4793e0ac7a3faccdc8c9c
Adenote <Agbajeanofiazeez@gmail.com>
2022-12-23 11:52:23 -0800

    strings length

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,15 +4,14 @@
  *_strlen -  function that returns the length of a string
  * @s: this is entry
  * Return: void
- * 
- *
  */
 
 int _strlen(char *s)
 {
 	int i = 0;
 
-	while (s[i]);
-	i++;
+	while (s[i])
+		i++;
+
 	return (i);
 }
